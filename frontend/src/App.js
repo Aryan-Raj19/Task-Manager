8e992fdd32ba0ede8d66c84f9e91bf38d088148d
Aryan-Raj19 <aryanraj19715@gmail.com>
2025-06-15 04:57:35 +0530

    modified:   frontend/.gitignore 	modified:   frontend/src/App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import TaskForm from './Components/TaskForm.js';
 import TaskList from './Components/TaskList.js';
 
-const API_URL = 'http://localhost:5000/tasks';
+const API_URL = process.env.REACT_APP_API_URL;
 
 function App() {
   const [tasks, setTasks] = useState([]);
